7beb973f9b8447fd99446a51225ce7bc8ed42449
hajjamn <hajjamn@gmail.com>
2024-03-27 12:57:28 +0100

    ADD: html structure and js objects array

--- js/app.js
+++ js/app.js
@@ -1,11 +1,51 @@
-console.log('ciao mondo')
-
 const { createApp } = Vue
 
 createApp({
   data() {
     return {
-      message: 'Vue'
+      message: 'Vue',
+      toDoList: [
+        {
+          text: 'Creare repo su Github',
+          done: false,
+        },
+        {
+          text: 'Clonare repo su VS Code',
+          done: false,
+        },
+        {
+          text: 'Creare index.html',
+          done: false,
+        },
+        {
+          text: 'Creare app.css in cartella css',
+          done: false,
+        },
+        {
+          text: 'Creare app.js in cartella js',
+          done: false,
+        },
+        {
+          text: 'Collegare index.html a app.css e app.js',
+          done: false,
+        },
+        {
+          text: 'Collegare Bootstrap',
+          done: false,
+        },
+        {
+          text: 'Collegare VUE',
+          done: false,
+        },
+        {
+          text: 'Collegare FontAwesome',
+          done: false,
+        },
+        {
+          text: 'First commmit',
+          done: false,
+        },
+      ],
     }
   }
 }).mount('#app')
